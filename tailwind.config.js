19bb8cc347d009e943a98bf3de975491e3e5fb97
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-10-11 09:56:29 -0300

    Configurado dependencias restantes do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import withMT from '@material-tailwind/react/utils/withMT'
+
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -129,4 +131,4 @@ export default {
     },
   },
   plugins: [],
-}
+})
